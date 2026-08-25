8b67bea6f4703a185bac6d3819405f93123a4cf8
Quang Nghia Trinh <quangnghia.trinh@shopee.com>
2026-08-25 16:48:49 +0800

    chore: update border radius for tab

--- packages/app/src/screens/workspace/workspace-screen.tsx
+++ packages/app/src/screens/workspace/workspace-screen.tsx
@@ -4339,7 +4339,7 @@ const styles = StyleSheet.create((theme) => ({
   },
   tab: {
     paddingHorizontal: theme.spacing[3],
-    paddingVertical: theme.spacing[2],
+    paddingVertical: 0,
     borderRadius: theme.borderRadius.md,
     flexDirection: "row",
     alignItems: "center",
